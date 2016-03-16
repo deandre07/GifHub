cf8f007b0f177662e94a7260fd4734b8e4d4da39
Andrew Levine <andrewl@onprem.com>
2016-03-16 02:03:02 -0500

    Start building separate bundlers for github/bitbucket

--- build/browserify.js
+++ build/browserify.js
@@ -2,15 +2,29 @@ import gulp from 'gulp';
 import browserify from 'browserify';
 import source from 'vinyl-source-stream';
 import buffer from 'vinyl-buffer';
+import mergeStream from 'merge-stream';
 
 gulp.task('browserify', ['clean'], function() {
+    const sites = {
+        github: 'src/js/sites/GitHub/index.js',
+        bitbucket: 'src/js/sites/BitBucket/index.js',
+    };
+
+    const streams = Object.keys(sites).forEach(site => (
+        browserifyPipe(site, sites[site], 'dist/chrome')
+    ));
+
+    return mergeStream.apply(this, streams);
+});
+
+function browserifyPipe(name, inPath, outFolder) {
     return browserify({
-        entries: ['src/js/sites/GitHub/index.js'],
+        entries: inPath,
         debug: true
     }).transform('babelify')
     .transform('brfs')
     .bundle()
-    .pipe(source('bundle.js'))
+    .pipe(source(`${name}.js`))
     .pipe(buffer())
-    .pipe(gulp.dest('dist/chrome'));
-});
+    .pipe(gulp.dest(outFolder));
+}
